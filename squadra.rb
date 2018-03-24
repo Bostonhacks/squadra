395b300fce600501139a00baca936d9519b77833
Andrew Hoang <ahoang18@bu.edu>
2018-03-24 11:59:54 -0400

    Get circle working, validate

--- squadra.rb
+++ squadra.rb
@@ -1,8 +1,12 @@
 require 'dotenv/load'
 
+puts 'Validating team.yml...'
+
+require_relative 'validate'
+
 puts 'Initiating team update run'
 
-# require_relative 'github'
+require_relative 'github'
 require_relative 'mailgun'
 
 puts 'Team update run occured, exiting'
