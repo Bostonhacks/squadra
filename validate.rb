10177f35b1275b82ae936467c796481e5f8d1288
Rudhra Raveendran <rudytheninja@gmail.com>
2020-03-31 13:50:13 -0400

    Improve email validations

--- validate.rb
+++ validate.rb
@@ -3,8 +3,9 @@ config = YAML.load_file('team.yml')
 
 puts "~~~ Validating team.yml ~~~"
 
-# Shitty validation layer. TODO: make better
 members = config['members']
+routes = config['routes']
+routes.map! { |route| route.name }
 
 members.each do |member|
   # TODO: make sure github user actually exists
@@ -12,11 +13,11 @@ members.each do |member|
   raise 'Missing github username for user' if member['github']['username'].nil?
   raise 'Invalid github role for user' unless %w[admin member].include?(member['github']['role'])
 
-	# TODO: make sure email is formatted correctly
-  # TODO: make sure email routes are written correctly
   raise 'Missing sendgrid entry for user' if member['sendgrid'].nil?
   raise 'Missing sendgrid email for user' if member['sendgrid']['email'].nil?
+	raise 'Invalid sendgrid email for user' unless URI::MailTo::EMAIL_REGEXP.match?(member['sendgrid']['email'])
   raise 'No sendgrid routes specified for user' unless member['sendgrid']['routes'].any?
+	raise 'Invalid sendgrid routes for user' unless (member['sendgrid']['routes'] - routes).empty?
 end
 
 puts "~~~ Finished validating team.yml ~~~"
