4df6fab7ddc2a751d6e3d4c3902d4d81a636c4f0
Rudhra Raveendran <rudytheninja@gmail.com>
2020-03-31 14:12:04 -0400

    Make the error useful

--- validate.rb
+++ validate.rb
@@ -9,15 +9,15 @@ routes.map! { |route| route['name'] }
 
 members.each do |member|
   # TODO: make sure github user actually exists
-  raise 'Missing github entry for user' if member['github'].nil?
-  raise 'Missing github username for user' if member['github']['username'].nil?
-  raise 'Invalid github role for user' unless %w[admin member].include?(member['github']['role'])
+  raise "Missing github entry for user: #{member['name']}" if member['github'].nil?
+  raise "Missing github username for user: #{member['name']}" if member['github']['username'].nil?
+  raise "Invalid github role for user: #{member['name']}" unless %w[admin member].include?(member['github']['role'])
 
-  raise 'Missing sendgrid entry for user' if member['sendgrid'].nil?
-  raise 'Missing sendgrid email for user' if member['sendgrid']['email'].nil?
-	raise 'Invalid sendgrid email for user' unless URI::MailTo::EMAIL_REGEXP.match?(member['sendgrid']['email'])
-  raise 'No sendgrid routes specified for user' unless member['sendgrid']['routes'].any?
-	raise 'Invalid sendgrid routes for user' unless (member['sendgrid']['routes'] - routes).empty?
+  raise "Missing sendgrid entry for user: #{member['name']}" if member['sendgrid'].nil?
+  raise "Missing sendgrid email for user: #{member['name']}" if member['sendgrid']['email'].nil?
+	raise "Invalid sendgrid email for user: #{member['name']}" unless URI::MailTo::EMAIL_REGEXP.match?(member['sendgrid']['email'])
+  raise "No sendgrid routes specified for user: #{member['name']}" unless member['sendgrid']['routes'].any?
+	raise "Invalid sendgrid routes for user: #{member['name']}" unless (member['sendgrid']['routes'] - routes).empty?
 end
 
 puts "~~~ Finished validating team.yml ~~~"
